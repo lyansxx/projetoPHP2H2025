5602ec0122114f2ce432d346144a84d4469fb00d
lyansxx <feliipesx7@gmail.com>
2025-09-09 20:16:59 -0300

    exercicio 01 e 02

--- ex.php
+++ ex.php
@@ -1,24 +1,24 @@
 <?php 
-//ex001
-// $valor1=10;
-// $valor2=8;
-// $resul=0;
+/*ex001
+$valor1=10;
+$valor2=8;
+$resul=0;
 
-//  $resul = $valor1 + $valor2;
-// if ($resul >= 20) {
-//   $resul +=8;
-//   echo("O valor da conta é $resul <br/>");
+ $resul = $valor1 + $valor2;
+if ($resul >= 20) {
+  $resul +=8;
+  echo("O valor da conta é $resul <br/>");
 
-// }
+}
 
-// else{
-// 	$resul -=5;
-// 	echo("o avlor da conta é $resul <br/>");
-// }
+else{
+	$resul -=5;
+	echo("o avlor da conta é $resul <br/>");
+}*/
 
 //ex02
 
-$numero = 55;
+/*$numero = 55;
 
 
 if($numero % 2 == 0 ){
@@ -29,6 +29,9 @@ if($numero % 2 == 0 ){
 else{
 	echo("Numero não divisivel por 10, 5 e 2");
 
-}
+}*/
+
+
+
 
  ?>
