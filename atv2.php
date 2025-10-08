b58cb6c2bc619a372bee1468cd237027fffe4cb5
Felipe Freitas <168735147+lyansxx@users.noreply.github.com>
2025-10-07 22:08:53 -0300

    ex 03

--- atv2.php
+++ atv2.php
@@ -27,33 +27,53 @@
     echo "</pre>";*/
 
     // ex 02
-    $i = 0;
- 		$s = 0;
- 		$arrayA = array (
- 					array(25,12,35),
- 					array(85,47,99),
- 					array(32,35,8)
- 								);
+    // $i = 0;
+ 	// 	$s = 0;
+ 	// 	$arrayA = array (
+ 	// 				array(25,12,35),
+ 	// 				array(85,47,99),
+ 	// 				array(32,35,8)
+ 	// 							);
 
-        $arrayB = array (
-                    array(25,12,35),
-                    array(85,47,99),
-                    array(32,35,8)
-                                );
+    //     $arrayB = array (
+    //                 array(25,12,35),
+    //                 array(85,47,99),
+    //                 array(32,35,8)
+    //                             );
 
- 		$resultado =  array();
+ 	// 	$resultado =  array();
 
- 		for ($i=0; $i <3 ; $i++) { 
- 			for ($v=0; $v <3; $v++) { 
- 			$resultado[$i] [$v]  = $arrayA [$i] [$v] + $arrayB[$i] [$v];
- 			}
+ 	// 	for ($i=0; $i <3 ; $i++) { 
+ 	// 		for ($v=0; $v <3; $v++) { 
+ 	// 		$resultado[$i] [$v]  = $arrayA [$i] [$v] + $arrayB[$i] [$v];
+ 	// 		}
  		
- 			}
+ 	// 		}
  		
 
- 		 echo "<pre>";
-        print_r($resultado);
-        echo "</pre>";
+ 	// 	 echo "<pre>";
+    //     print_r($resultado);
+    //     echo "</pre>";
+
+
+    // ex 03
+
+    $Alunos = array[ 
+                    ["nome" => "Felipe", "nota"=> 9],
+                    ["nome" => "João","nota" => 9],
+                    ["nome" => "Bianca","nota" => 10],
+                    ["nome" => "Any","nota" => 9], 
+                    ["nome" => "José","nota" => 3]
+                    ];
+
+            $notastotal= 0;
+            $media = 0;
+            $nome = ''; 
+
+            foreach ($Alunos as $Alunos) {
+                $notastotal += $Alunos [""nota"];
+                
+            }
 
  		
   ?>
