94966c3bbc4936c48d374c567ea8540696aaded3
Felipe Freitas <168735147+lyansxx@users.noreply.github.com>
2025-10-07 21:37:07 -0300

    ex 02

--- atv2.php
+++ atv2.php
@@ -26,32 +26,34 @@
     print_r($resultado);
     echo "</pre>";*/
 
-$i = 0;
+    // ex 02
+    $i = 0;
  		$s = 0;
  		$arrayA = array (
  					array(25,12,35),
  					array(85,47,99),
  					array(32,35,8)
  								);
+
+        $arrayB = array (
+                    array(25,12,35),
+                    array(85,47,99),
+                    array(32,35,8)
+                                );
+
  		$resultado =  array();
 
  		for ($i=0; $i <3 ; $i++) { 
  			for ($v=0; $v <3; $v++) { 
- 			$resultado[$i] [$v]  = $arrayA [$i] [$v] + $arrayA[$i] [$v];
+ 			$resultado[$i] [$v]  = $arrayA [$i] [$v] + $arrayB[$i] [$v];
  			}
  		
  			}
  		
 
- 		echo "<pre>";
-    print_r($resultado);
-    echo "</pre>";
+ 		 echo "<pre>";
+        print_r($resultado);
+        echo "</pre>";
 
- 		// $arrayB = array (
- 		// 			   array(98,65,35),
- 		// 			   array(5,27,9,),
- 		// 			   array(74,14,3)
- 		// 							);
-
-
+ 		
   ?>
