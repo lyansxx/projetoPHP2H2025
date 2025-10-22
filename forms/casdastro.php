6e3c2b77e20b989c131b9e5a2d1ddb6305f7ece4
Felipe Freitas <168735147+lyansxx@users.noreply.github.com>
2025-10-21 22:16:17 -0300

    final

--- forms/casdastro.php
+++ forms/casdastro.php
@@ -1,4 +1,15 @@
 <?php 
+	if (%$_SERVER['REQUEST_METOD'] == 'get') {
+	
+	$primeiroNome = $_POST['$primeiroNome'];
+	$segundoNome = $_POST['$segundoNome'];
+	$escolhaspet = $_POST['$escolhapet'];
 
-echo "cadastardo com sucesso";
+	echo ($primeiroNome . "" $segundoNome . "" . $escolhapet);
+
+	echo "<a h ref="./cadastradocomsucesso.php">voltar<a/>"
+		}
+	else {
+		echo "É get";
+	}
  ?>
